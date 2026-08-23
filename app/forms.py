5f3c163ddda6025e6c745ab7329733fe8b8bac2e
Murad.H <devoopsdb@gmail.com>
2026-08-24 00:15:31 +0400

    feat(forms): CableLineItem formset; clean header form; drop broken clean_title and inline styles
    - CableCalForm reduced to header fields (order_num, transport, overrides)
    - CableLineItemForm + CableLineItemFormSet for multi-row entry
    - ReelsListForm/TransportListForm: form-control/form-select classes, no inline styles
    - Remove dead/broken clean_title methods (subscripted a method -> TypeError)

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,46 @@
+# pyright: reportArgumentType=false, reportAttributeAccessIssue=false
 from django import forms
+from django.forms import formset_factory
 
-from .models import *
+from .models import CableCal, CableLineItem, ReelsList, TransportList
+
+
+class CableCalForm(forms.ModelForm):
+    """Order header: identity, transport, optional per-calc overrides."""
+
+    class Meta:
+        model = CableCal
+        fields = ["order_num", "transport", "margin_override", "packing_override"]
+        widgets = {
+            "order_num": forms.TextInput(attrs={"class": "form-control"}),
+            "transport": forms.Select(attrs={"class": "form-select"}),
+            "margin_override": forms.NumberInput(
+                attrs={"class": "form-control", "step": "0.1"}
+            ),
+            "packing_override": forms.NumberInput(
+                attrs={"class": "form-control", "step": "0.01"}
+            ),
+        }
+
+
+class CableLineItemForm(forms.ModelForm):
+    """One cable line in an order (entered via a formset)."""
+
+    class Meta:
+        model = CableLineItem
+        fields = ["cod", "name", "con_num", "order_len", "max_len", "mass", "diameter"]
+        widgets = {
+            "cod": forms.TextInput(attrs={"class": "form-control"}),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "con_num": forms.NumberInput(attrs={"class": "form-control"}),
+            "order_len": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "max_len": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "mass": forms.NumberInput(attrs={"class": "form-control", "step": "0.001"}),
+            "diameter": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+        }
+
+
+CableLineItemFormSet = formset_factory(CableLineItemForm, extra=1, can_delete=True)
 
 
 class ReelsListForm(forms.ModelForm):
@@ -18,65 +58,16 @@ class ReelsListForm(forms.ModelForm):
             "max_load",
         ]
         widgets = {
-            "name": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 200px; border: 0px; height: 30px;",
-                }
-            ),
-            "height": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "width": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "diameter": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "diameter_neck": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "length_neck": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "reel_type": forms.Select(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 150px; border: 0px; height: 30px;",
-                }
-            ),
-            "mass": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "max_load": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "height": forms.NumberInput(attrs={"class": "form-control"}),
+            "width": forms.NumberInput(attrs={"class": "form-control"}),
+            "diameter": forms.NumberInput(attrs={"class": "form-control"}),
+            "diameter_neck": forms.NumberInput(attrs={"class": "form-control"}),
+            "length_neck": forms.NumberInput(attrs={"class": "form-control"}),
+            "reel_type": forms.Select(attrs={"class": "form-select"}),
+            "mass": forms.NumberInput(attrs={"class": "form-control"}),
+            "max_load": forms.NumberInput(attrs={"class": "form-control"}),
         }
-
-        def clean_title(self):
-            name = self.clean_title["name"]
-            return name
 
 
 class TransportListForm(forms.ModelForm):
@@ -84,108 +75,9 @@ class TransportListForm(forms.ModelForm):
         model = TransportList
         fields = ["name", "length", "width", "height", "max_load"]
         widgets = {
-            "name": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 300px; border: 0px; height: 30px;",
-                }
-            ),
-            "length": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 100px; border: 0px; height: 30px;",
-                }
-            ),
-            "width": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 100px; border: 0px; height: 30px;",
-                }
-            ),
-            "height": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 100px; border: 0px; height: 30px;",
-                }
-            ),
-            "max_load": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 100px; border: 0px; height: 30px;",
-                }
-            ),
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "length": forms.NumberInput(attrs={"class": "form-control"}),
+            "width": forms.NumberInput(attrs={"class": "form-control"}),
+            "height": forms.NumberInput(attrs={"class": "form-control"}),
+            "max_load": forms.NumberInput(attrs={"class": "form-control"}),
         }
-
-        def clean_title(self):
-            name = self.clean_title["name"]
-            return name
-
-
-class CableCalForm(forms.ModelForm):
-    class Meta:
-        model = CableCal
-        fields = [
-            "order_num",
-            "cod",
-            "name",
-            "mass",
-            "diameter",
-            "con_num",
-            "order_len",
-            "max_len",
-            "transport",
-        ]
-        widgets = {
-            "order_num": forms.TextInput(
-                attrs={"class": "form-input", "style": "width: 200px; height: 25px;"}
-            ),
-            "cod": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 150px; border: 0px; height: 30px;",
-                }
-            ),
-            "name": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 200px; border: 0px; height: 30px;",
-                }
-            ),
-            "mass": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "diameter": forms.TextInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "con_num": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "order_len": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "max_len": forms.NumberInput(
-                attrs={
-                    "class": "form-input",
-                    "style": "width: 70px; border: 0px; height: 30px;",
-                }
-            ),
-            "transport": forms.Select(
-                attrs={"class": "form-input", "style": "width: 120px; height: 25px;"}
-            ),
-        }
-
-        def clean_title(self):
-            order_num = self.clean_title["order_num"]
-            return order_num
